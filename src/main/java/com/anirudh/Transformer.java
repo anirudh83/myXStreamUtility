cf0ba50e38c1faafbc2b05af207170757251eeb2
anirudh <anirudh@panthacorp.com>
2014-07-15 01:30:23 +0530

    Moved Object creation to Factory

--- src/main/java/com/anirudh/Transformer.java
+++ src/main/java/com/anirudh/Transformer.java
@@ -10,53 +10,31 @@ import com.thoughtworks.xstream.io.xml.StaxDriver;
  */
 public class Transformer<T> {
 
-    private static final XStream XSTREAM_INSTANCE = null;
 
     public T getObjectFromJSON(String json){
-        return (T) getInstance().fromXML(json);
+        return (T) XStreamObjectFactory.getJettisonDriverInstance().fromXML(json);
     }
 
     public String getJSONFromObject(T t){
-        return getInstance().toXML(t);
+        return XStreamObjectFactory.getJettisonDriverInstance().toXML(t);
     }
 
-    private XStream getInstance(){
-        if(XSTREAM_INSTANCE==null){
-            return new XStream(new JettisonMappedXmlDriver());
-        }  else {
-            return XSTREAM_INSTANCE;
-        }
-    }
 
     public T getObjectFromXML(String xml){
-        return (T)getStaxDriverInstance().fromXML(xml);
+        return (T)XStreamObjectFactory.getStaxDriverInstance().fromXML(xml);
     }
 
     public String getXMLFromObject(T t){
-        return getStaxDriverInstance().toXML(t);
+        return XStreamObjectFactory.getStaxDriverInstance().toXML(t);
     }
 
     public T getObjectFromXMLUsingDomDriver(String xml){
-        return (T)getDomDriverInstance().fromXML(xml);
+        return (T)XStreamObjectFactory.getDomDriverInstance().fromXML(xml);
     }
 
     public String getXMLFromObjectUsingDomDriver(T t){
-        return getDomDriverInstance().toXML(t);
+        return XStreamObjectFactory.getDomDriverInstance().toXML(t);
     }
 
-    private XStream getStaxDriverInstance(){
-        if(XSTREAM_INSTANCE==null) {
-            return new XStream(new StaxDriver());
-        }else{
-            return XSTREAM_INSTANCE;
-        }
-    }
 
-    private XStream getDomDriverInstance(){
-        if(XSTREAM_INSTANCE==null){
-            return new XStream(new DomDriver());
-        }else{
-            return XSTREAM_INSTANCE;
-        }
-    }
 }
--- src/main/java/com/anirudh/XStreamObjectFactory.java
+++ src/main/java/com/anirudh/XStreamObjectFactory.java
@@ -1,9 +1,38 @@
 package com.anirudh;
 
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.json.JettisonMappedXmlDriver;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+import com.thoughtworks.xstream.io.xml.StaxDriver;
+
 /**
  * Created by anirudh on 15/07/14.
  */
 public class XStreamObjectFactory {
 
+    private static final XStream XSTREAM_INSTANCE = null;
 
+    public static XStream getStaxDriverInstance(){
+        if(XSTREAM_INSTANCE==null) {
+            return new XStream(new StaxDriver());
+        }else{
+            return XSTREAM_INSTANCE;
+        }
+    }
+
+    public static XStream getDomDriverInstance(){
+        if(XSTREAM_INSTANCE==null){
+            return new XStream(new DomDriver());
+        }else{
+            return XSTREAM_INSTANCE;
+        }
+    }
+
+    public static XStream getJettisonDriverInstance(){
+        if(XSTREAM_INSTANCE==null){
+            return new XStream(new JettisonMappedXmlDriver());
+        }  else {
+            return XSTREAM_INSTANCE;
+        }
+    }
 }
